1d2a2b6933013578b207fe17eaab9c40cba08fec
House <houserabbit@163.com>
2015-12-14 23:36:56 +0800

    debug

--- Testing.py
+++ Testing.py
@@ -47,10 +47,10 @@ class Speed_Overload_Testing:
             row_former = reader_iter.next()
             row_later = reader_iter.next()
             former_time = util.str_time_to_second(row_former[TIME_POSITION_IN_CSV])
-            former_mileage = float(row_former[-1].split(":")[1].split("k")[0])
+            former_mileage = float(row_former[-5].split(":")[1].split("k")[0])
             former_type = row_former[ROAD_TYPE_POSITION_IN_CSV]
             later_time = util.str_time_to_second(row_later[TIME_POSITION_IN_CSV])
-            later_mileage = float(row_later[-1].split(":")[1].split("k")[0])
+            later_mileage = float(row_later[-5].split(":")[1].split("k")[0])
             # print time_later
             # print later_minite, former_minite, later_second, former_second
             # 做两个循环外变量,否则捕捉异常时会出错
@@ -72,7 +72,7 @@ class Speed_Overload_Testing:
                     time_former = time_later
                     row_later = reader_iter.next()
                     later_time = util.str_time_to_second(row_later[TIME_POSITION_IN_CSV])
-                    later_mileage = float(row_later[-1].split(":")[1].split("k")[0])
+                    later_mileage = float(row_later[-5].split(":")[1].split("k")[0])
                     distance_later = later_mileage - former_mileage
                     time_later = later_time - former_time
                     v_former = 0
